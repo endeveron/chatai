eb9ba89edd3c7e4035e1623cd82c6bb6d10fd3fc
Dmitry <endeveron@gmail.com>
2024-05-20 18:42:36 +0300

    add a metadata to the main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,50 @@ import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
   title: 'Chat AI',
+  applicationName: 'Chat AI',
   description: 'Conversational chatbot with LLM',
   openGraph: {
     title: 'Chat AI',
     description: 'Conversational chatbot with LLM',
     siteName: 'Chat AI',
+    images: [
+      {
+        url: 'https://chatai-sigma-three.vercel.app/assets/icons/icon-512x512.png',
+        width: 512,
+        height: 512,
+      },
+      {
+        url: 'https://chatai-sigma-three.vercel.app/assets/icons/icon-256x256.png', // Must be an absolute URL
+        width: 256,
+        height: 256,
+      },
+    ],
+    locale: 'en_US',
     type: 'website',
+  },
+  icons: {
+    icon: [
+      { url: '/assets/icons/icon.svg' },
+      new URL(
+        '/assets/icons/icon.svg',
+        'https://chatai-sigma-three.vercel.app'
+      ),
+      // { url: '/icon-dark.png', media: '(prefers-color-scheme: dark)' },
+    ],
+    shortcut: ['/favicon.ico'],
+    apple: [
+      {
+        url: '/assets/icons/icon-180x180.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+    ],
+    other: [
+      {
+        rel: 'apple-touch-icon-precomposed',
+        url: '/apple-touch-icon-precomposed.png',
+      },
+    ],
   },
 };
 
